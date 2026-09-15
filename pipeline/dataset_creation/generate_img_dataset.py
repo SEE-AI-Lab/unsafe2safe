eb2cc764e2c8c05f092a361ba4d23a8545c0a9ca
Minh Dinh <minh.dinhtrong@aalto>
2026-09-15 13:05:46 -0400

    Fix dataset generator import

--- pipeline/dataset_creation/generate_img_dataset.py
+++ pipeline/dataset_creation/generate_img_dataset.py
@@ -18,7 +18,7 @@ sys.path.append("./stable_diffusion")
 
 from ldm.modules.attention import CrossAttention
 from ldm.util import instantiate_from_config
-from metrics.clip_similarity import ClipSimilarity
+from pipeline.metrics.clip_similarity import ClipSimilarity
 
 
 ################################################################################
